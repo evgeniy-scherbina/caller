3903f3e08079d514113bd0d8c1996d84d7a543c2
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-10-28 20:47:20 -0400

    Decrease CPU & Memory

--- deploy/terraform/ecs.tf
+++ deploy/terraform/ecs.tf
@@ -10,8 +10,8 @@ resource "aws_ecs_task_definition" "server" {
   family                   = "server-${local.service_name}-${local.environment}"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 256
+  memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
 
